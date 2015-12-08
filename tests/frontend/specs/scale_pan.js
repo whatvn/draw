2557850f177a1ae47081d069459e586098f5c5e2
Jack Farley <jack@meldce.com>
2015-12-08 23:20:44 +0000

    - Updated panning test to not check for pan limit - Reformatted spec file to use spaces instead of tabs

--- tests/frontend/specs/scale_pan.js
+++ tests/frontend/specs/scale_pan.js
@@ -1,19 +1,18 @@
 describe("Scale and Pan", function(){
-
   var oldPadName,
       padName,
       path,
       reloaded = false;
 
-	var panMethods = {
-		'middle click and drag': {
-			button: 1
-		},
-		'<CTRL> click and drag': {
-			button: 0,
-			ctrlKey: true
-		}
-	};
+  var panMethods = {
+    'middle click and drag': {
+      button: 1
+    },
+    '<CTRL> click and drag': {
+      button: 0,
+      ctrlKey: true
+    }
+  };
 
   it("creates a pad", function(done) {
     padName = helper.newPad(done);
@@ -31,128 +30,132 @@ describe("Scale and Pan", function(){
     done();
   });
  
- 	describe('Panning', function() {
-		var m;
+   describe('Panning', function() {
+    var m;
 
-		for (m in panMethods) {
-			it(m + " pans linearly", (function(m) { return function(done) {
-				this.timeout(5000);
+    for (m in panMethods) {
+      it(m + " pans linearly", (function(m) { return function(done) {
+        this.timeout(5000);
 
-				var chrome$ = helper.padChrome$;
-				var paper = window.frames[0].paper;
+        var chrome$ = helper.padChrome$;
+        var paper = window.frames[0].paper;
+        var view = paper.project.view;
+        var startBounds = view.bounds;
 
-				// Mouse clicks and drags to create path
-				var canvas = chrome$("#myCanvas");
-				canvas.simulate('drag', {
-					dx: -100, dy: -50,
-					eventOptions: panMethods[m]
-				});
+        // Mouse clicks and drags to create path
+        var canvas = chrome$("#myCanvas");
+        canvas.simulate('drag', {
+          dx: -100, dy: -50,
+          eventOptions: panMethods[m]
+        });
 
-				var view = paper.project.view;
-				var bounds = view.bounds;
-				expect(bounds.x).to.be(100);
-				expect(bounds.y).to.be(50);
-				done();
-			}; })(m));
+        var view = paper.project.view;
+        var bounds = view.bounds;
+        expect(bounds.x).to.be(startBounds.x + 100);
+        expect(bounds.y).to.be(startBounds.y + 50);
+        done();
+      }; })(m));
 
-			it(m + " stops at 0,0", (function(m) { return function(done) {
-				this.timeout(5000);
+      it(m + " pans backwards too", (function(m) { return function(done) {
+        this.timeout(5000);
 
-				var chrome$ = helper.padChrome$;
-				var paper = window.frames[0].paper;
+        var chrome$ = helper.padChrome$;
+        var paper = window.frames[0].paper;
+        var view = paper.project.view;
+        var startBounds = view.bounds;
 
-				// Mouse clicks and drags to create path
-				var canvas = chrome$("#myCanvas");
-				canvas.simulate('drag', {
-					dx: 200, dy: 100,
-					eventOptions: panMethods[m]
-				});
-				var view = paper.project.view;
-				var bounds = view.bounds;
-				expect(bounds.x).to.be(0);
-				expect(bounds.y).to.be(0);
-				done();
-			}; })(m));
-		}
-	});
+        // Mouse clicks and drags to create path
+        var canvas = chrome$("#myCanvas");
+        canvas.simulate('drag', {
+          dx: 70, dy: 50,
+          eventOptions: panMethods[m]
+        });
+        var view = paper.project.view;
+        var bounds = view.bounds;
+        expect(bounds.x).to.be(startBounds.x - 70);
+        expect(bounds.y).to.be(startBounds.y - 50);
+        done();
+      }; })(m));
+    }
+  });
 
-	var scrollTypes = {
-		'pixel scroll': {
-			eventOptions : {
-				deltaX: -50,
-				deltaY: 0,
-				deltaMode: 0
-			},
-			scaleDiff: 50 * 0.002
-		},
-		'line scroll': {
-			eventOptions : {
-				deltaX: 5,
-				deltaY: 0,
-				deltaMode: 1
-			},
-			scaleDiff: -5 * 0.02
-		},
-		'page scroll': {
-			eventOptions : {
-				deltaX: -1,
-				deltaY: 0,
-				deltaMode: 2
-			},
-			scaleDiff: 1 * 0.1
-		}
-	};
+  var scrollTypes = {
+    'pixel scroll': {
+      eventOptions : {
+        deltaX: -50,
+        deltaY: 0,
+        deltaMode: 0
+      },
+      scaleDiff: 50 * 0.002
+    },
+    'line scroll': {
+      eventOptions : {
+        deltaX: 5,
+        deltaY: 0,
+        deltaMode: 1
+      },
+      scaleDiff: -5 * 0.02
+    },
+    'page scroll': {
+      eventOptions : {
+        deltaX: -1,
+        deltaY: 0,
+        deltaMode: 2
+      },
+      scaleDiff: 1 * 0.1
+    }
+  };
 
-	describe('Scaling', function() {
-		var s;
+  describe('Scaling', function() {
+    var s;
 
-		for (s in scrollTypes) {
-			it(s + ' should scale', (function(s) { return function(done) {
-				this.timeout(5000);
+    for (s in scrollTypes) {
+      it(s + ' should scale', (function(s) { return function(done) {
+        this.timeout(5000);
 
-				var chrome$ = helper.padChrome$;
-				var paper = window.frames[0].paper;
-				var view = paper.project.view;
-				var oldScale = view.zoom;
+        var chrome$ = helper.padChrome$;
+        var paper = window.frames[0].paper;
+        var view = paper.project.view;
+        var oldScale = view.zoom;
 
-				// Mouse clicks and drags to create path
-				var canvas = chrome$("#myCanvas");
-				canvas.simulate('wheel', scrollTypes[s].eventOptions);
+        // Mouse clicks and drags to create path
+        var canvas = chrome$("#myCanvas");
+        canvas.simulate('wheel', scrollTypes[s].eventOptions);
 
-				var newScale = view.zoom;
+        var newScale = view.zoom;
 
-				expect(newScale).to.be(oldScale + scrollTypes[s].scaleDiff);
-				done();
-			}; })(s));
-		}
-	});
+        expect(newScale).to.be(oldScale + scrollTypes[s].scaleDiff);
+        done();
+      }; })(s));
+    }
+  });
 
-	describe('Scaled panning', function() {
-		it('should pan evenly when zoomed in', function(done) {
-				var chrome$ = helper.padChrome$;
-				var paper = window.frames[0].paper;
-				var view = paper.project.view;
-				view.zoom = 2;
-				view.draw();
-				
-				var startBounds = view.bounds;
+  describe('Scaled panning', function() {
+    it('should pan evenly when zoomed in', function(done) {
+        var chrome$ = helper.padChrome$;
+        var paper = window.frames[0].paper;
+        var view = paper.project.view;
+        view.zoom = 2;
+        view.draw();
+        
+        var startBounds = view.bounds;
 
-				// Mouse clicks and drags to create path
-				var canvas = chrome$("#myCanvas");
+        // Mouse clicks and drags to create path
+        var canvas = chrome$("#myCanvas");
 
-				canvas.simulate('drag', {
-					dx: -200, dy: -100,
-					eventOptions: {
-						button: 1
-					}
-				});
-				
-				var bounds = view.bounds;
-				
-				expect(bounds.x).to.be(startBounds.x + (200/2));
-				expect(bounds.y).to.be(startBounds.y + (100/2));
-				done();
-		
-		});
-	});
+        canvas.simulate('drag', {
+          dx: -200, dy: -100,
+          eventOptions: {
+            button: 1
+          }
+        });
+        
+        var bounds = view.bounds;
+        
+        expect(bounds.x).to.be(startBounds.x + (200/2));
+        expect(bounds.y).to.be(startBounds.y + (100/2));
+        done();
+    
+    });
+  });
 });
